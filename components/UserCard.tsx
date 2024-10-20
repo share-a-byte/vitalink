2fe27766a242783be38aa258d2182d0659d905df
kathryn <leekathryn943@gmail.com>
2024-10-20 07:29:37 -0500

    added some color to info portions of page

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -21,10 +21,19 @@ const UserCard: React.FC<UserCardProps> = ({ user }) => {
         style={styles.userImage}
       />
       <View style={styles.userDetails}>
-        <Text style={styles.userName}>{user.name}</Text>
-        <Text style={styles.ageText}>Age: {user.age}</Text>
-        <Text style={styles.birthdateText}>Birthdate: {user.birthdate}</Text>
-        <Text style={styles.phoneText}>Phone: {user.phoneNumber}</Text>
+      <Text style={styles.userName}>{user.name}</Text>
+        <View style={styles.infoRow}>
+          <Text style={styles.infoText}>Age:</Text>
+          <Text style={styles.ageText}>{user.age}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.infoText}>Birthdate:</Text>
+          <Text style={styles.birthdateText}>{user.birthdate}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.infoText}>Phone:</Text>
+          <Text style={styles.phoneText}>{user.phoneNumber}</Text>
+        </View>
       </View>
       <TouchableOpacity style={styles.contactButton}>
         <Text style={styles.contactButtonText}>Contact User</Text>
@@ -36,6 +45,7 @@ const UserCard: React.FC<UserCardProps> = ({ user }) => {
 const styles = StyleSheet.create({
   cardContainer: {
     flex: 1, 
+    borderRadius: 10,
     backgroundColor: "#FFFFFF",
     justifyContent: "center", 
     alignItems: "center", 
@@ -69,6 +79,17 @@ const styles = StyleSheet.create({
     color: "#6B7280",
     fontSize: 16,
     marginBottom: 16,
+  },
+  infoText: { 
+    fontSize: 14,
+    color: "#10B981",
+    fontWeight: "bold",
+    marginRight: 8, 
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "flex-start", 
+    marginBottom: 4, 
   },
   contactButton: {
     backgroundColor: "#1F2937",
