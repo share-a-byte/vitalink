48eabd4553ca87f1b58ab1a1ef2bd46a688cd473
Abigail Gillham <abigailg0314@gmail.com>
2024-10-20 13:58:47 -0500

    Added dropdown to users

--- app/users.tsx
+++ app/users.tsx
@@ -1,7 +1,11 @@
 // app/users.tsx
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import UserCard from "../components/UserCard";
+import SymptomCard from "../components/SymptomCard";
+import EnterSymptom from "../components/EnterSymptom";
+import { Dropdown } from "react-native-element-dropdown";
+import { addSymptomToFirestore } from "../src/symptomservices";
 
 const user = {
   id: "1",
@@ -11,11 +15,42 @@ const user = {
   phoneNumber: "(123) 345-456",
 };
 
+interface Symptom {
+  id: string;
+  name: string;
+}
+
+// const symptoms: Symptom[] = [
+//   { id: "1", name: "Type II Diabetes" },
+//   { id: "2", name: "High Blood Pressure" },
+// ];
+
+// const SymptomList: React.FC<{ userId: string }> = ({ userId }) => {
+//   useEffect(() => {
+//     const addSymptoms = async () => {
+//       for (const symptom of symptoms) {
+//         await addSymptomToFirestore(userId, symptom);
+//       }
+//     };
+
+//     addSymptoms();
+//   }, [userId]);
+
+//   return (
+//     <ScrollView style={styles.scrollviewpadding}>
+//       {symptoms.map((symptom) => (
+//         <SymptomCard key={symptom.id} symptomCard={symptom} />
+//       ))}
+//     </ScrollView>
+//   );
+// };
+
 export default function UserProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Profile</Text>
       <UserCard user={user} />
+      <EnterSymptom /> {}
     </View>
   );
 }
@@ -31,5 +66,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 16,
+  },
+  scrollviewpadding: {
+    marginBottom: 70,
   },
 });
